dd29e58ad90698b3b05603c85f1159d31b287a83
Takao Noguchi <tkngch@runbox.com>
2026-06-29 15:19:14 +0100

    Add Sudoku puzzle solver

---
+++ internal/solver/solve.go
@@ -0,0 +1,123 @@
+package solver
+
+import (
+	"errors"
+
+	"github.com/tkngch/sudoku-go/internal/puzzle"
+)
+
+var ErrSolutionNotFound = errors.New("solution not found")
+
+// Return a solved copy of the grid, or ErrSolutionNotFound if the grid has no
+// solution. The input grid is not modified.
+func Solve(grid *puzzle.Grid) (*puzzle.Grid, error) {
+	grid = grid.Clone() // do not mutate the input argument
+	dropAllInvalidCandidates(grid)
+	if isSolved(grid) {
+		return grid, nil
+	}
+
+	return searchSolution(grid)
+}
+
+func isSolved(grid *puzzle.Grid) bool {
+	for cell := range grid.Cells() {
+		if cell.Candidates().Count() != 1 {
+			return false
+		}
+		for peer := range grid.PeersOf(cell.Position()) {
+			if cell.Candidates() == peer.Candidates() {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// Mutate the grid to drop invalid candidates for all cells
+func dropAllInvalidCandidates(grid *puzzle.Grid) {
+	for {
+		isAnyValueDropped := false
+
+		for cell := range grid.Cells() {
+			isDropped := dropInvalidCandidates(grid, cell)
+			isAnyValueDropped = isAnyValueDropped || isDropped
+		}
+
+		if !isAnyValueDropped {
+			return
+		}
+	}
+}
+
+// Mutate the grid to drop invalid candidates for the cell
+func dropInvalidCandidates(grid *puzzle.Grid, cell puzzle.Cell) bool {
+	invalidCandidates := puzzle.NewSingleCandidate(0)
+	for peer := range grid.PeersOf(cell.Position()) {
+		if peer.Candidates().Count() == 1 {
+			invalidCandidates = invalidCandidates.Union(peer.Candidates())
+		}
+	}
+
+	newCandidates := cell.Candidates().Remove(invalidCandidates)
+	if cell.Candidates() != newCandidates {
+		grid.Set(cell.Position(), newCandidates)
+		return true
+	}
+
+	return false
+}
+
+func searchSolution(grid *puzzle.Grid) (*puzzle.Grid, error) {
+	// 1. Find the cell that has more than one candidates.
+	cell, isFound := findUnfilledCell(grid)
+	if !isFound && isSolved(grid) {
+		return grid, nil
+	} else if !isFound {
+		return grid, ErrSolutionNotFound
+	}
+
+	// 2. Pick one of its candidates and replace the cell
+	for value := range cell.Candidates().All() {
+		newGrid := grid.Clone()
+		newGrid.Set(cell.Position(), value)
+
+		// 3. Update the peers and the others
+		dropAllInvalidCandidates(newGrid)
+
+		solved, err := searchSolution(newGrid)
+		if err == nil {
+			return solved, nil
+		} else if !errors.Is(err, ErrSolutionNotFound) {
+			return solved, err
+		}
+	}
+
+	return grid, ErrSolutionNotFound
+}
+
+// Find the cell that has the smallest number of candidates among the cells
+// which has more than one candidates
+func findUnfilledCell(grid *puzzle.Grid) (puzzle.Cell, bool) {
+	isFound := false
+	var foundCell puzzle.Cell
+
+	for cell := range grid.Cells() {
+		count := cell.Candidates().Count()
+		switch count {
+		case 0: // search went down the path without solution. fail early.
+			return foundCell, false
+		case 1:
+			continue
+		case 2:
+			return cell, true
+		default:
+			if !isFound || count < foundCell.Candidates().Count() {
+				isFound = true
+				foundCell = cell
+			}
+		}
+	}
+
+	return foundCell, isFound
+}
---
+++ internal/solver/solve_test.go
@@ -0,0 +1,121 @@
+package solver_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tkngch/sudoku-go/internal/puzzle"
+	"github.com/tkngch/sudoku-go/internal/solver"
+)
+
+func TestSolve(t *testing.T) {
+	testCases := []struct {
+		name     string
+		puzzle   []string
+		solution []string
+	}{
+		{
+			name: "4x4 without backtracking",
+			puzzle: []string{
+				".234",
+				"3.12",
+				"43.1",
+				"214.",
+			},
+			solution: []string{
+				"1234",
+				"3412",
+				"4321",
+				"2143",
+			},
+		},
+		{
+			name: "9x9 without backtracking",
+			puzzle: []string{
+				"812753649",
+				"940080170",
+				"005491203",
+				"054237090",
+				"369845020",
+				"207069504",
+				"521970360",
+				"438526917",
+				"796318452",
+			},
+			solution: []string{
+				"812753649",
+				"943682175",
+				"675491283",
+				"154237896",
+				"369845721",
+				"287169534",
+				"521974368",
+				"438526917",
+				"796318452",
+			},
+		},
+		{
+			name: "9x9 with backtracking",
+			puzzle: []string{
+				"800000000",
+				"003600000",
+				"070090200",
+				"050007000",
+				"000045700",
+				"000100030",
+				"001000068",
+				"008500010",
+				"090000400",
+			},
+			solution: []string{
+				"812753649",
+				"943682175",
+				"675491283",
+				"154237896",
+				"369845721",
+				"287169534",
+				"521974368",
+				"438526917",
+				"796318452",
+			},
+		},
+		{
+			name: "4x4 with no solution",
+			puzzle: []string{
+				"11..",
+				"....",
+				"....",
+				"....",
+			},
+			solution: nil,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(
+			testCase.name,
+			func(t *testing.T) {
+				testSolve(t, testCase.puzzle, testCase.solution)
+			},
+		)
+	}
+}
+
+func testSolve(t *testing.T, input, solution []string) {
+	grid, err := puzzle.Parse(strings.Join(input, ""))
+	require.NoErrorf(t, err, "could not parse [%v]", input)
+
+	actual, err := solver.Solve(grid)
+	if solution == nil {
+		require.ErrorIs(t, err, solver.ErrSolutionNotFound)
+		return
+	}
+	require.NoError(t, err, "could not find a solution")
+
+	expected, err := puzzle.Parse(strings.Join(solution, ""))
+	require.NoErrorf(t, err, "could not parse [%v]", solution)
+
+	assert.Equalf(t, expected.String(), actual.String(), "expected\n%s\nactual\n%s", expected.Render(), actual.Render())
+}
